bc6a4981b8669930991084b5b7fbbf8fab16e081
Victor <victor@lagerfors.com>
2020-04-07 10:49:03 +0200

    added code skeleton

--- src/main/SimFurutaPendulum.java
+++ src/main/SimFurutaPendulum.java
@@ -1,8 +1,29 @@
 package main;
 
+
+// Class for simulating and animating the Furuta pendulum (available in furuta.jar)
 public class SimFurutaPendulum {
 
-	public SimFurutaPendulum(double x, double y){
+	// Creates and starts a new process simulation, with initial
+	// pendulum angle theta0 and arm angle phi0 (in radians)
+	public SimFurutaPendulum(double theta0, double phi0){
+		
+	}
+
+	// Returns the pendulum angle (in radians)
+	public double getThetaAngle(){
+		return 0.0;
+	}
+
+	// Returns the arm angle (in radians)
+	public double getPhiAngle(){
+
+		return 0.0;
+	}
+
+	// Sets the control signal (unitless, between -1.0 and +1.0)
+	public void setControlSignal(double u){
 
 	}
 }
+
